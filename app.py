29af40fe712fa8894cb2763844577d78739b69e9
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-07-21 15:20:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,67 +1,159 @@
-#import streamlit as st
-#from bokeh.models.widgets import Button
-#from bokeh.models import CustomJS
-#from streamlit_bokeh_events import streamlit_bokeh_events
-#import cv2
-#import numpy as np
-#import pytesseract
-#from PIL import Image
-
 import streamlit as st
-from bokeh.models.widgets import Button
-from bokeh.models import CustomJS
-from streamlit_bokeh_events import streamlit_bokeh_events
+import os
+import time
+import glob
+import os
+import cv2
+import numpy as np
+import pytesseract
+from PIL import Image
 
-st.title("Interfaces Multimodales Audio y Texto")
+from gtts import gTTS
+from googletrans import Translator
 
-st.write("Interfaz de texto a Audio")
-text = st.text_input("Que decir?")
 
-tts_button = Button(label="Decirlo", width=100)
 
-tts_button.js_on_event("button_click", CustomJS(code=f"""
-    var u = new SpeechSynthesisUtterance();
-    u.text = "{text}";
-    u.lang = 'es-es';   
 
-    speechSynthesis.speak(u);
-    """))
 
-st.bokeh_chart(tts_button)
+st.title("Reconocimiento óptico de Caracteres")
 
-st.write("Interfaz de Audio a texto")
-stt_button = Button(label="Habla", width=100)
+img_file_buffer = st.camera_input("Toma una Foto")
 
-stt_button.js_on_event("button_click", CustomJS(code="""
-    var recognition = new webkitSpeechRecognition();
-    recognition.continuous = true;
-    recognition.interimResults = true;
- 
-    recognition.onresult = function (e) {
-        var value = "";
-        for (var i = e.resultIndex; i < e.results.length; ++i) {
-            if (e.results[i].isFinal) {
-                value += e.results[i][0].transcript;
-            }
-        }
-        if ( value != "") {
-            document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
-        }
-    }
-    recognition.start();
-    """))
+with st.sidebar:
+      filtro = st.radio("Aplicar Filtro",('Con Filtro', 'Sin Filtro'))
 
-result = streamlit_bokeh_events(
-    stt_button,
-    events="GET_TEXT",
-    key="listen",
-    refresh_on_update=False,
-    override_height=75,
-    debounce_time=0)
 
-if result:
-    if "GET_TEXT" in result:
-        st.write(result.get("GET_TEXT"))
+if img_file_buffer is not None:
+    # To read image file buffer with OpenCV:
+    bytes_data = img_file_buffer.getvalue()
+    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+    
+    if filtro == 'Con Filtro':
+         cv2_img=cv2.bitwise_not(cv2_img)
+    else:
+         cv2_img= cv2_img
+    
+        
+    img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
+    text=pytesseract.image_to_string(img_rgb)
+    st.write(text) 
+
+
+
+
+try:
+    os.mkdir("temp")
+except:
+    pass
+#st.title("Text to speech")
+translator = Translator()
+
+#text = st.text_input("Enter text")
+in_lang = st.selectbox(
+    "Select your input language",
+    ("English", "Spanish", "Bengali", "korean", "Chinese", "Japanese"),
+)
+if in_lang == "English":
+    input_language = "en"
+elif in_lang == "Spanish":
+    input_language = "es"
+elif in_lang == "Bengali":
+    input_language = "bn"
+elif in_lang == "korean":
+    input_language = "ko"
+elif in_lang == "Chinese":
+    input_language = "zh-cn"
+elif in_lang == "Japanese":
+    input_language = "ja"
+
+out_lang = st.selectbox(
+    "Select your output language",
+    ("English", "Spanish", "Bengali", "korean", "Chinese", "Japanese"),
+)
+if out_lang == "English":
+    output_language = "en"
+elif out_lang == "Spanish":
+    output_language = "es"
+elif out_lang == "Bengali":
+    output_language = "bn"
+elif out_lang == "korean":
+    output_language = "ko"
+elif out_lang == "Chinese":
+    output_language = "zh-cn"
+elif out_lang == "Japanese":
+    output_language = "ja"
+
+english_accent = st.selectbox(
+    "Select your english accent",
+    (
+        "Default",
+        "India",
+        "United Kingdom",
+        "United States",
+        "Canada",
+        "Australia",
+        "Ireland",
+        "South Africa",
+    ),
+)
+
+if english_accent == "Default":
+    tld = "com"
+elif english_accent == "India":
+    tld = "co.in"
+
+elif english_accent == "United Kingdom":
+    tld = "co.uk"
+elif english_accent == "United States":
+    tld = "com"
+elif english_accent == "Canada":
+    tld = "ca"
+elif english_accent == "Australia":
+    tld = "com.au"
+elif english_accent == "Ireland":
+    tld = "ie"
+elif english_accent == "South Africa":
+    tld = "co.za"
+
+
+def text_to_speech(input_language, output_language, text, tld):
+    translation = translator.translate(text, src=input_language, dest=output_language)
+    trans_text = translation.text
+    tts = gTTS(trans_text, lang=output_language, tld=tld, slow=False)
+    try:
+        my_file_name = text[0:20]
+    except:
+        my_file_name = "audio"
+    tts.save(f"temp/{my_file_name}.mp3")
+    return my_file_name, trans_text
+
+
+display_output_text = st.checkbox("Display output text")
+
+if st.button("convert"):
+    result, output_text = text_to_speech(input_language, output_language, text, tld)
+    audio_file = open(f"temp/{result}.mp3", "rb")
+    audio_bytes = audio_file.read()
+    st.markdown(f"## Your audio:")
+    st.audio(audio_bytes, format="audio/mp3", start_time=0)
+
+    if display_output_text:
+        st.markdown(f"## Output text:")
+        st.write(f" {output_text}")
+
+
+def remove_files(n):
+    mp3_files = glob.glob("temp/*mp3")
+    if len(mp3_files) != 0:
+        now = time.time()
+        n_days = n * 86400
+        for f in mp3_files:
+            if os.stat(f).st_mtime < now - n_days:
+                os.remove(f)
+                print("Deleted ", f)
+
+
+remove_files(7)
         
 
 
