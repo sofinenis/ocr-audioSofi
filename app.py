e7af19cc8bac0a0802a1b651b38ffc22ea64dd68
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-07-21 11:55:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,49 @@ from bokeh.models.widgets import Button
 from bokeh.models import CustomJS
 from streamlit_bokeh_events import streamlit_bokeh_events
 
-st.title("Interfaces Multimodales Audio y Texto")
+import streamlit as st
+import cv2
+import numpy as np
+import pytesseract
+from PIL import Image
 
-st.write("Interfaz de texto a Audio")
-text = st.text_input("Que decir?")
 
-tts_button = Button(label="Decirlo", width=100)
+st.title("Reconocimiento óptico de Caracteres")
 
-tts_button.js_on_event("button_click", CustomJS(code=f"""
-    var u = new SpeechSynthesisUtterance();
-    u.text = "{text}";
-    u.lang = 'es-es';   
+img_file_buffer = st.camera_input("Toma una Foto")
 
-    speechSynthesis.speak(u);
-    """))
+with st.sidebar:
+      filtro = st.radio("Aplicar Filtro",('Con Filtro', 'Sin Filtro'))
 
-st.bokeh_chart(tts_button)
+
+if img_file_buffer is not None:
+    # To read image file buffer with OpenCV:
+    bytes_data = img_file_buffer.getvalue()
+    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+    
+    if filtro == 'Con Filtro':
+         cv2_img=cv2.bitwise_not(cv2_img)
+    else:
+         cv2_img= cv2_img
+    
+        
+    img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
+    text=pytesseract.image_to_string(img_rgb)
+    st.write(text) 
+
+    st.title("Interfaces Multimodales Audio y Texto")
+
+    st.write("Interfaz de texto a Audio")
+#text = st.text_input("Que decir?")
+
+    tts_button = Button(label="Decirlo", width=100)
+
+    tts_button.js_on_event("button_click", CustomJS(code=f"""
+        var u = new SpeechSynthesisUtterance();
+        u.text = "{text}";
+        u.lang = 'es-es';   
+
+        speechSynthesis.speak(u);
+         """))
+
+     st.bokeh_chart(tts_button)
