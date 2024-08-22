15b9adc2da09c49dc79f29daa43eef489e14ea03
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-08-22 12:36:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ with st.sidebar:
 bg_image = st.sidebar.file_uploader("Cargar Imagen:", type=["png", "jpg"])
 if bg_image is not None:
     img_cv = cv2.imread(r'/content/parrafo.png')
-    img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
-    st.write(pytesseract.image_to_string(img_rgb))  
+    #img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
+    st.write(pytesseract.image_to_string(img_cv))  
     
       
 if img_file_buffer is not None:
