10ffdedc205b2b685d5b076a687972770de3c6b0
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-08-22 12:49:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ with st.sidebar:
 
 bg_image = st.sidebar.file_uploader("Cargar Imagen:", type=["png", "jpg"])
 if bg_image is not None:
-    
+    uploaded_file=bg_image
     st.image(uploaded_file, caption='Imagen cargada.', use_column_width=True)
     
     # Guardar la imagen en el sistema de archivos
